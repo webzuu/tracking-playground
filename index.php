0d580c4fdf3bec2a1fb3275969c93825f19d3573
webzuu <info@webzuu.pl>
2017-10-30 20:28:15 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+  $analyticsId = 'UA-108868666-1';
+  $uri = $_SERVER['REQUEST_URI'];
+?>
+<!doctype html>
+<html>
+  <head>
+    <title><?=$uri ?></title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css" />
+
+    <script src="/tracking.js"></script>
+
+    <script type="text/javascript">
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '<?=$analyticsId;?>', 'auto', {'allowLinker':true});
+      ga('require', 'displayfeatures');
+      ga('require', 'linker');
+      ga('send', 'pageview');
+    </script>
+  </head>
+  <body>
+
+    <div class="card">
+      <div class="card-body">
+        <div class="card-title">
+          Klasyczny kod ga (przeładowanie strony)
+        </div>
+        <div class="card-text">
+          <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="/default-page1">Default page 1</a></li>
+            <li class="nav-item"><a class="nav-link" href="/default-page2">Default page 2</a></li>
+            <li class="nav-item"><a class="nav-link" href="/default-page3">Default page 3</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-body">
+        <div class="card-title">
+          Javascript ga('set','page'); ga('send','pageview')
+        </div>
+        <div class="card-text">
+          <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="/js-page1" data-track-js>JS page 1</a></li>
+            <li class="nav-item"><a class="nav-link" href="/js-page2" data-track-js>JS page 2</a></li>
+            <li class="nav-item"><a class="nav-link" href="/js-page3" data-track-js>JS page 3</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ tracking.js
@@ -0,0 +1,20 @@
+;(function($){
+
+  $(function() {
+
+    var links = $('[data-track-js]');
+
+    links.each(function(i, e) {
+      $(e).on('click', function(evt) {
+        var ga = window.ga;
+        evt.preventDefault();
+        var href = $(evt.currentTarget).attr('href');
+        history.pushState(null, null, href);
+        ga('set', 'page', href);
+        ga('send', 'pageview');
+      });
+    });
+
+  });
+
+})(jQuery);
